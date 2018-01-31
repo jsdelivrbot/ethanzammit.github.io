b20611b2f126f6e7be6885a8c2750acfc2694b7b
Ethan Zammit <ethanzammit@gmail.com>
2018-01-31 14:53:14 +0000

    Adding color gradients lul

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -2,10 +2,18 @@
 /*global width*/
 /*global ellipse*/
 /*global height*/
+/*global color*/
+/*global fill*/
+/*global stroke*/
+/*global keyCode*/
+/*global saveCanvas*/
 
 var NUM_CIRCLES = 12;
 var circleDiameter;
 var circleRadius;
+var rVal;
+var gVal;
+var bVal;
 
 
 function setup(){
@@ -17,31 +25,49 @@ function setup(){
 
 
 function draw() {
+    
+    rVal = 255;
+    gVal = 0;
+    bVal = 0;
+
  var isShifted = false;
- 
-  
+     
+    
+    
   var y = 0;
   while (y <= height){
-  
-  
   
   var x;
   
   if(isShifted) {
-      x = circleRadius
+    x = circleRadius
   }
   else{
-      x = 0;
+    x = 0;
   }
-  
-  
+
   
   
   while (x <= width) {
+    fill(color(rVal, gVal, bVal));
+    stroke(color(rVal, gVal, bVal));
     ellipse(x, y, circleDiameter, circleDiameter);
     x = x + circleDiameter;
     }
     y = y+ circleRadius;
     isShifted = !isShifted;
+    
+    rVal = rVal - 2;
+    gVal = gVal + 7;
+    bVal = bVal + 3;
+    
   }
+}
+
+
+function keyPressed() {
+  if (keyCode === 115 || keyCode === 83) {
+    saveCanvas('geometricPattern', 'png');
+  }
+  return false;
 }
